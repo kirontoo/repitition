49eb05258ce427de02ee7f1d0c458629cc92c37b
Amy Dang <kirontoo@gmail.com>
2023-12-08 14:08:19 -0800

    apply theme

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { NavigationRoute } from './screens/types';
-import { StyleSheet } from 'react-native';
 import WorkoutsScreen from './screens/Workouts';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DetailsScreen from './screens/Details';
@@ -9,7 +8,7 @@ import WorkoutScreen from './screens/Workout';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ExerciseForm from './screens/ExerciseForm';
 import ExerciseList from './screens/ExerciseList';
-import { TamaguiProvider } from 'tamagui';
+import { Theme, TamaguiProvider } from 'tamagui';
 import config from './tamagui.config';
 import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
@@ -33,40 +32,23 @@ export default function App() {
   }
   return (
     <TamaguiProvider config={config}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: '#f4511e',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-              },
-            }}
-          >
-            <Stack.Screen name={NavigationRoute.Home} component={WorkoutsScreen}
-              options={{
-                title: 'My Workouts',
-              }} />
-            <Stack.Screen name={NavigationRoute.Details} component={DetailsScreen} />
-            <Stack.Screen name={NavigationRoute.WorkoutForm} component={WorkoutFormScreen} />
-            <Stack.Screen name={NavigationRoute.ExerciseForm} component={ExerciseForm} />
-            <Stack.Screen name={NavigationRoute.ExerciseList} component={ExerciseList} />
-            <Stack.Screen name={NavigationRoute.WorkoutScreen} component={WorkoutScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <Theme name="dark">
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name={NavigationRoute.Home} component={WorkoutsScreen}
+                options={{
+                  title: 'My Workouts',
+                }} />
+              <Stack.Screen name={NavigationRoute.Details} component={DetailsScreen} />
+              <Stack.Screen name={NavigationRoute.WorkoutForm} component={WorkoutFormScreen} />
+              <Stack.Screen name={NavigationRoute.ExerciseForm} component={ExerciseForm} />
+              <Stack.Screen name={NavigationRoute.ExerciseList} component={ExerciseList} />
+              <Stack.Screen name={NavigationRoute.WorkoutScreen} component={WorkoutScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </Theme>
     </TamaguiProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
