45f8cee5d923541e8bab761a7212930524d9c420
Amy Dang <kirontoo@gmail.com>
2024-01-11 15:29:07 -0800

    set up basic ui for timer

---
+++ app/timer.tsx
@@ -0,0 +1,63 @@
+import { Text, XStack, YStack, useTheme, Button } from "tamagui";
+import { Stack } from "expo-router";
+import useTimer from "../hooks/useTimer";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default function TimerScreen() {
+  const timer = useTimer(30 * 1000);
+  const inset = useSafeAreaInsets();
+  const theme = useTheme();
+
+  // TODO:
+  const formatToMinAndSec = (init: number) : string => {
+    return "";
+  }
+
+  return (
+    <YStack
+      padding="$2"
+      paddingTop={inset.top}
+      paddingBottom={inset.bottom}
+      alignItems="center"
+      justifyContent="space-between"
+      height="100%"
+    >
+      <Stack.Screen options={{ title: "timer", headerShown: false }} />
+      <XStack justifyContent="flex-end" width="100%" marginTop="$1">
+        <Button unstyled variant="outlined">
+          <MaterialIcons name="more-vert" size={16} color={theme.color.val} />
+        </Button>
+      </XStack>
+
+      <YStack alignItems="center" justifyContent="center" space="$4">
+        <AnimatedCircularProgress
+          size={200}
+          width={8}
+          tintColor={theme.color8.val}
+          fill={(timer.currentTime / (30 * 1000)) * 100}
+          onAnimationComplete={() => console.log("onAnimationComplete")}
+        >
+          {() => <Text fontSize="$10">{timer.currentTime / 1000}</Text>}
+        </AnimatedCircularProgress>
+        <Button circular>
+          <MaterialIcons name="pause" size={16} color={theme.color.val} />
+        </Button>
+      </YStack>
+
+      <XStack
+        justifyContent="space-between"
+        width="100%"
+        alignItems="center"
+        marginBottom="$2"
+      >
+        <Text fontSize="$5">2/10</Text>
+        <Button variant="outlined" size="$2" unstyled>
+          <MaterialIcons name="skip-next" size={20} color={theme.color.val} />
+        </Button>
+      </XStack>
+    </YStack>
+  );
+}
+
